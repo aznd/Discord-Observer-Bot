0ae4547bcfb57d89b1097f2d99155f4ebe192a73
aznd <jakob.stechow@protonmail.com>
2021-12-26 01:24:15 +0100

    Yep

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import config
 
 TOKEN = ''
+
+@tasks.loop()
+async def inform(days=config.DAYS):
+    pass
+    member: discord.Member = discord.utils.get()
+    for role in config.ROLES_TO_LOOK_FOR:
+        await member.add_roles(config.ROLES_TO_LOOK_FOR)
