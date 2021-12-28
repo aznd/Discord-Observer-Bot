b7a3191de587811816c4b0e682d9b4bbe6e255ab
aznd <jakob.stechow@protonmail.com>
2021-12-29 00:05:32 +0100

    finished.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands, tasks
+from discord.utils import async_all
 import config
 import os
 import datetime
@@ -10,37 +11,48 @@ load_dotenv()
 keep_alive()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = commands.Bot(command_prefix="-")
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix=".", intents=intents)
 
 # Convert days to hours cause we can only use hours.
 # hours = int(config.DAYS) * 24
 
-level = logging.INFO
-fmt = '[%levelname)s] %(asctime)s - %(message)s'
+level = logging.WARNING
+fmt = '[%(levelname)s] %(asctime)s - %(message)s'
 logging.basicConfig(level=level, format=fmt)
-
 
 @tasks.loop(hours=24)
 async def inform(ctx):
-    all_members_in_guild = ctx.guild.members
-    for member in all_members_in_guild:
+    members = await ctx.guild.fetch_members().flatten()
+    for member in members:
         roles_of_member = member.roles
         for role in roles_of_member:
             if str(role) in config.ROLES_TO_LOOK_FOR:
                 first_time = datetime.datetime.now()
                 joined_time = member.joined_at
                 diff = first_time - joined_time
-                channel: discord.TextChannel = client.get_channel(925051225714597999)
-                await channel.send(f"INFO: (Guest) {member.name} has now been on the server for {str(diff.days)} days")
-                if str(diff.days) >= str(config.DAYS):
-                    await channel.send(f"IMPORTANT: Guest {member.name} has now been on the server for {str(diff.days)} days.")
-
-
+                channel: discord.TextChannel = client.get_channel(id=config.CHANNEL_ID)
+                if diff.days <= 30:
+                    await channel.send(f"INFO: {member.name} has now been on the server for {str(diff.days)} days")
+                if diff.days >= config.DAYS:
+                    if diff.days <= 30:
+                        await channel.send(f"IMPORTANT: {member.name} has now been on the server for {str(diff.days)} days.")
 
 @client.command()
 async def start(ctx):
     await ctx.send("Task starting...")
     inform.start(ctx)
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, number):
+    await ctx.message.delete()
+    channel = ctx.message.channel
+    messages = await channel.history(limit=int(number)).flatten()
+    await channel.delete_messages(messages)
+    
 
 @client.event
 async def on_ready():
